3a2fd09d755d653fbdcc12a3c5122a23f8b2bace
Ariola Qeleposhi <ariolaqeleposhi@hotmail.co.uk>
2018-04-17 20:43:51 +0100

    Updating content in files into one python file

--- pybooks_code.py
+++ pybooks_code.py
@@ -11,9 +11,7 @@ def search_books():
     search_term = form_data["text"]
     r = requests.get("https://www.googleapis.com/books/v1/volumes?q={}&key=AIzaSyCbhIgJKBPeTDIhAF4MCY0VXOZoTX3IcAc".format(search_term))
     results = search(r)
-    print(results)
     return render_template('searchResults.html', results=results)
-
 
 
 def search(data):
@@ -36,8 +34,35 @@ def search(data):
 
     return results
 
+
 @app.route('/')
 def home():
     return render_template('homePage.html')
 
+
 app.run(debug=True)
+
+
+def find(data):
+    results = []
+    titles = []
+    results = []
+    book_data = data.json()
+    books = book_data['items']
+
+    for book in books:
+        result = {}
+        volumeInfo = book.get('volumeInfo')
+        result["title"] = volumeInfo.get('title')
+        # result["author"] = volumeInfo.get('authors')[0]
+        # result["description"] = volumeInfo.get('description')
+        results.append(result)
+
+    return results
+
+
+def search_books(title):
+    r = requests.get("https://www.googleapis.com/books/v1/volumes?q={}&key=AIzaSyCbhIgJKBPeTDIhAF4MCY0VXOZoTX3IcAc".format(title))
+    book_list = find(r)
+
+    return book_list
